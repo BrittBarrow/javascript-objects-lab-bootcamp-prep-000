3407798d81c4e34a73bf92ed1bee5ea0f3713182
Brittany Barrow <bangelabarrow@gmail.com>
2018-03-28 23:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {key: 'value'});
+  var key = 'value'
+  return recipes[key] = 'value';
 }
